187c0991e181457fbf9d79dbdf62e74f7de46df7
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-05-28 19:04:08 +0300

    Configure Prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,26 @@
-import './globals.css'
 import { Inter } from 'next/font/google'
+
+import Header from '@components/Header'
+
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Restaurant App',
+	title: 'Restaurant App'
 }
 
 export default function RootLayout({
-  children,
+	children
 }: {
-  children: React.ReactNode
+	children: React.ReactNode
 }) {
-  return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
-  )
+	return (
+		<html lang='en'>
+			<body className={inter.className}>
+				<Header />
+				{children}
+			</body>
+		</html>
+	)
 }
