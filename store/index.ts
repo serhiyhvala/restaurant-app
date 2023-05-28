6d37089d408e598046be1fdcf9d10d10e13a6cac
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-05-29 01:14:16 +0300

    Add products slice and create custom typed hooks

--- store/index.ts
+++ store/index.ts
@@ -1,11 +1,16 @@
 import { configureStore } from '@reduxjs/toolkit'
+import productsSlice from '@store/slices/productsSlice'
 
 import { productsApi } from '@services/index'
 
 export const store = configureStore({
 	reducer: {
+		product: productsSlice,
 		[productsApi.reducerPath]: productsApi.reducer
 	},
 	middleware: getDefaultMiddleware =>
 		getDefaultMiddleware().concat(productsApi.middleware)
 })
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
