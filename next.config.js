f0485f4203c5d6cc771d3c60361a11c3d693883c
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-05-29 01:12:13 +0300

    Update config for images

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "products-api-p7cr.onrender.com",
+        port: '',
+        pathname: "/**"
+      }
+    ]
+  }
+}
 
 module.exports = nextConfig
